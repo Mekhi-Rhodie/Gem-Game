313b17950664478d13357d564caf32d094e433f6
Mekhi-Rhodie <mekhirhodie@gmail.com>
2020-01-20 19:50:35 -0500

    Chagne value of yellow gem so that it will always be one

--- scripts/app.js
+++ scripts/app.js
@@ -2,11 +2,12 @@ const targetNumber = _.random(130, 250);
 let points = 0;
 let rounds = 3;
 let strikes = 3;
+let score = 0;
 
-$("#red-gem").attr("value", _.random(1, 5))
+$("#red-gem").attr("value", _.random(2, 5))
 $("#blue-gem").attr("value", _.random(10, 20))
 $("#green-gem").attr("value", _.random(5, 10))
-$("#yellow-gem").attr("value", _.random(1, 25))
+$("#yellow-gem").attr("value", 1) 
 
 $("#target-number").append(targetNumber)
 
@@ -15,6 +16,11 @@ $(document).ready(function () {
         event.preventDefault()
         const gemValue = parseInt($(this).attr("value"))
         $("#points").empty().append(points += gemValue)
+        if(points === targetNumber){
+            alert("You Won!")
+        }else if(points > targetNumber){
+            alert("You Lose!!!!!")
+        }
     });
 });
 
