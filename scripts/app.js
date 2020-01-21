7ae89f35f858425059712cde4b02ebdcca22e5aa
Mekhi-Rhodie <mekhirhodie@gmail.com>
2020-01-20 19:43:02 -0500

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,21 @@
-const randomTotal = _.random(130,250)
+const targetNumber = _.random(130, 250);
+let points = 0;
+let rounds = 3;
+let strikes = 3;
+
+$("#red-gem").attr("value", _.random(1, 5))
+$("#blue-gem").attr("value", _.random(10, 20))
+$("#green-gem").attr("value", _.random(5, 10))
+$("#yellow-gem").attr("value", _.random(1, 25))
+
+$("#target-number").append(targetNumber)
+
+$(document).ready(function () {
+    $("img").on("click", function (event) {
+        event.preventDefault()
+        const gemValue = parseInt($(this).attr("value"))
+        $("#points").empty().append(points += gemValue)
+    });
+});
+
+
