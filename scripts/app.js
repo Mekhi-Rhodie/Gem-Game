c811b07be7b8b52101fca4e24f8e4316fef865c2
Mekhi-Rhodie <mekhirhodie@gmail.com>
2020-01-22 21:45:42 -0500

    Declare variable that holds number of clicks

--- scripts/app.js
+++ scripts/app.js
@@ -3,23 +3,25 @@ let points = 0;
 let rounds = 3;
 let strikes = 0;
 let score = 0;
+let clicks = 0;
 
 $("#red-gem").attr("value", _.random(2, 5))
 $("#blue-gem").attr("value", _.random(10, 20))
 $("#green-gem").attr("value", _.random(5, 10))
-$("#yellow-gem").attr("value", _.random(1,2)) 
+$("#yellow-gem").attr("value", 1) 
 
 $("#target-number").append(targetNumber)
 
 $(document).ready(function () {
     $("img").on("click", function (event) {
         event.preventDefault()
+        clicks++
         const gemValue = parseInt($(this).attr("value"))
         $("#points").empty().append(points += gemValue)
         if(points === targetNumber){
             $("#container").css("opacity",".30")
             $("#win-modal").slideDown(1300).css("display","block")
-            //location.reload()
+            $(".modal-content").append("<p>" + "<strong>" +"Clicks:  "+ "</strong>" +clicks + "</p>")
         }else if(points > targetNumber){
             console.log("You Lose!!!!!")
             strikes++
